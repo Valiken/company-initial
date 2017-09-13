95c1af2e047adb92ac74ce0261d32e55a04d0c4a
Brian Berg <brian.berg@cgi.com>
2017-09-13 15:30:32 -0600

    test

--- src/main/java/io/rscale/training/company/MessageController.java
+++ src/main/java/io/rscale/training/company/MessageController.java
@@ -2,6 +2,7 @@ package io.rscale.training.company;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ public class MessageController {
     private String message;
 
     @RequestMapping("/message")
+    @Profile("cloud")
     String getMessage() {
         return "\n" + this.message + "\n\n";
     }
